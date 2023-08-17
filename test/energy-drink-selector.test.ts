d01fe5e8627d118d84de29d35ac73a714ae8ab63
lee <lpleepriest@gmail.com>
2023-08-17 19:10:36 +0100

    Initial commit

---
+++ test/energy-drink-selector.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EnergyDrinkSelector from '../lib/energy-drink-selector-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/energy-drink-selector-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EnergyDrinkSelector.EnergyDrinkSelectorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
